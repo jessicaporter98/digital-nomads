e27c4a980e6a47c6c0a7f376f73a42290cf79888
Jessica Porter <106319613+jessicaporter98@users.noreply.github.com>
2023-02-13 14:02:18 -0600

    class 6 edits

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ function myFunction() {
   document.getElementById("demo").innerHTML = txt;
 }
 
+
+function myFunction() {
+  var txt;
+  if (confirm(" Welcome jessicaporter98@yahoo.com you have successfully logged on !")) {
+    txt = "welcome jessicaporter98@yahoo.com";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+  document.getElementById(" welcome jessicaporter98@yahoo.com").innerHTML = txt;
+}
+
+
+
+
+
+
+if (confirm("Would you like to continue to a digital nomad's oasis ?")) {
+  document.write("Thanks for viewing :) ");
+} else {
+  document.write("OK, Goodbye");
+}
